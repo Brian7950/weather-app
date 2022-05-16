e76ebf645e55c898532276f87dc8e5d70632401f
Brian7950 <brianh7950@gmail.com>
2022-05-15 19:16:30 -0600

    added uv, humidity, wind

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,9 @@
 var apiKey = "4e92bc7b36552d5fe8e403251b632fd8";
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth() + 1;
+date = `${month}/${day}`;
+
 
 
 $("#search-weather").on("click",function(){
@@ -28,15 +33,20 @@ function getFiveDay(lat,lon, city){
             var currentForcast = `<div>
             <h1> City: ${city}</h1>
             <h6>Temp:${apiData.current.temp}<span><img src="https://openweathermap.org/img/wn/${apiData.current.weather[0].icon}@2x.png
-
             " /></span></h6>
+
+            <h6> UV: ${apiData.current.uvi}<span> "TEST" </span></h6>
+
+            <h6> Humidity: ${apiData.current.humidity}%</h6>
+
+            <h6> Wind: ${apiData.current.wind_speed} MPH</h6>
             
             </div>`
             $("#display-today").html(currentForcast)
 
             var fiveDay = ""
             for(let i = 0; i < 5; i++){
-                var day = moment().add(i+1, 'days').format()
+                var day = moment().add(i+1, 'days').format("MMMM Do");
                 fiveDay += `<div class="card">
                 ${day}
                 <h6>Temp:${apiData.daily[i].temp.day}<span><img src="https://openweathermap.org/img/wn/${apiData.daily[i].weather[0].icon}@2x.png
