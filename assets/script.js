c0dd378b2b1edc4ac3bda5ad16bf86ca431e0d77
Brian7950 <brianh7950@gmail.com>
2022-05-18 17:22:10 -0600

    adding uvi color

--- assets/script.js
+++ assets/script.js
@@ -3,109 +3,108 @@ var date = new Date();
 var day = date.getDate();
 var month = date.getMonth() + 1;
 date = `${month}/${day}`;
+
 previousCity = {};
 
-
-
-
-//search for city 
-$("#search-weather").on("click",function(){
-    var city = $("#city-name").val()
+//search for city
+$("#search-weather").on("click", function () {
+    var city = $("#city-name").val();
     console.log(city);
     getApi(city);
-    createHistoryEl(city); 
-
+    createHistoryEl(city);
 });
 
-
-function getApi(city){
-    var url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+function getApi(city) {
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     $.ajax({
-        url:url,
-        success:function(apiData){
+        url: url,
+        success: function (apiData) {
             console.log(apiData);
             var lat = apiData.coord.lat;
             var lon = apiData.coord.lon;
             getFiveDay(lat, lon, city);
-        }})
-};
+        },
+    });
+}
 
-function getFiveDay(lat,lon, city){
-    var url =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+function getFiveDay(lat, lon, city) {
+    var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     $.ajax({
-        url:url,
-        success:function(apiData){
+        url: url,
+        success: function (apiData) {
             console.log(apiData);
 
             //todays forcast in individual section
             var currentForcast = `<div>
             <h1> City: ${city.toUpperCase()}</h1>
-            <h6>Temp:${apiData.current.temp}<span><img src="https://openweathermap.org/img/wn/${apiData.current.weather[0].icon}@2x.png
+            <h6>Temp:${apiData.current.temp
+                }<span><img src="https://openweathermap.org/img/wn/${apiData.current.weather[0].icon
+                }@2x.png
             " /></span></h6>
 
-            <h6> UV: ${uvColor(apiData)}</h6>
+            <h6>UV:<span id="uv">  ${apiData.current.uvi}${uvColor(
+                    apiData
+                )}<span></h6>
+           
 
             <h6> Humidity: ${apiData.current.humidity}%</h6>
 
             <h6> Wind: ${apiData.current.wind_speed} MPH</h6>
             
-            </div>`
-            $("#display-today").html(currentForcast)
+            </div>`;
+            $("#display-today").html(currentForcast);
 
             //new section created for 5 day breakdown
-            var fiveDay = ""
-            for(let i = 0; i < 5; i++){
-                var day = moment().add(i+1, 'days').format("MMMM Do");
+            var fiveDay = "";
+            for (let i = 0; i < 5; i++) {
+                var day = moment()
+                    .add(i + 1, "days")
+                    .format("MMMM Do");
                 fiveDay += `<div class="card">
                 ${day}
                 <h6>Temp:${apiData.daily[i].temp.day}<span><img src="https://openweathermap.org/img/wn/${apiData.daily[i].weather[0].icon}@2x.png" />
-                </div>`
+                </div>`;
             }
             $("#five-day").html(fiveDay);
-        
-        }})   
-};
-
-function savedHistory(){
-    localStorage.setItem("previousCity", JSON.stringify(previousCity))
-};
-
-function loadHistory(){
-    previousCity = JSON.parse(localStorage.getItem("previousCity"));
-    //if empty, create an object 
-    if(!previousCity){
-        previousCity = {
-            city: []
-        };
-    }
-
+        },
+    });
 }
 
-//creates previous names already searched 
-function createHistoryEl(cityName){
+function savedHistory() {
+    localStorage.setItem("previousCity", JSON.stringify(previousCity));
+}
+
+function loadHistory() {
+    previousCity = JSON.parse(localStorage.getItem("previousCity"));
+    //if empty, create an object
+    if (!previousCity) {
+        previousCity = {
+            city: [],
+        };
+    }
+}
+
+//creates previous names already searched
+function createHistoryEl(cityName) {
     //create list of previous city searches
-    var cityLi = $("<li>").text(cityName)
-    .addClass("table-secondary");
+    var cityLi = $("<li>").text(cityName);
     $("#previous-search").append(cityLi);
 }
 
 //uv colors depending on uv scale 0-11
-function uvColor(data){
-    if(data){
-        console.log(data.current.uvi);
-        var uv = $("<li>").text(data.current.uvi).append(uv);
-    }
-    // if(data.current.uvi ){
-    //     var uvColorEl = $("<p>").text("test")
-    //     $("#display-today").append(uvColorEl);
-    //     console.log(data);
-    //     console.log(data.current.uvi);
-    }
-    // else if(data.current.uvi > 2){
-    //     var uvColorEl = $("<span>").text(data.current.uvi)
-    //     // .addClass("uv-high");
-    //     $("#display-today").append(uvColorEl);
-    //     console.log(data);
-    //     console.log(data.current.uvi);
-    // }
-// }
+function uvColor(data) {
+    if (data.current.uvi <= 2) {
+        var uvEl = document.getElementById("uv");
+        return uvEl.classList.add("uv-low");
+    } else if (data.current.uvi > 2 && data.current.uvi < 6) {
+        var uvEl = document.getElementById("uv");
+         uvEl.classList.add("uv-moderate");
+    } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
+        var uvEl = document.getElementById("uv");
+        return uvEl.classList.add("uv-high");
+    } else if (data.current.uvi >= 8 && data.current.uvi < 11) {
+        var uvEl = document.getElementById("uv");
+        return uvEl.classList.add("uv-veryhigh");
+    } else var uvEl = document.getElementById("uv");
+    return uvEl.classList.add("uv-extreme");
+}
